07d8b60aaedc887a245c92540ea7821645efec8f
Benjamin Hoesli <benjin8or@MacBook-Air-von-Benjamin.local>
2022-12-05 00:37:19 +0100

    Finished Journey Page (Static)

--- src/Pages/Journey.js
+++ src/Pages/Journey.js
@@ -1,9 +1,40 @@
 import React from 'react';
-import { Grid,TextField, InputLabel } from '@material-ui/core';
+import { Grid,TextField } from '@material-ui/core';
 import { Button} from 'react-bootstrap';
-import Select from 'react-select'
+import MenuItem from '@mui/material/MenuItem';
+import InputAdornment from '@mui/material/InputAdornment';
+
+const cars = [
+    {
+      value: 'Porsche',
+      label: 'Porsche Taycan',
+    },
+    {
+      value: 'Tesla',
+      label: 'Tesla Model 3',
+    },
+    {
+      value: 'Hyundai',
+      label: 'Hyundai Ioniq 5',
+    },
+    {
+      value: 'Audi',
+      label: 'Audi e-tron GT',
+    },
+    {
+    value: 'Honda',
+    label: 'Honda e',
+  },
+  ];
+
+
 
 export default function Journey() {
+    const [car, setCar] = React.useState('Tesla');
+    const handleChange = (event) => {
+        setCar(event.target.value);
+      };
+    
     return (
         <div style={{ 
             backgroundImage: `url("https://images.unsplash.com/photo-1489641024260-20e5cb3ee4aa?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2233&q=80")` ,
@@ -15,13 +46,28 @@ export default function Journey() {
     
     <form>
         <Grid>
-            <Grid container>
-                <Grid item xs={6}><TextField id="origin" label="Origin" variant="filled" /></Grid>
-                <Grid item xs={6}><InputLabel id="carmodel" label="Car Model" /><Select> </Select></Grid>
-                <Grid item xs={6}><TextField id="destination" label="Destination" variant="filled" /></Grid>
-                <Grid item xs={6}><TextField id="charge" label="Remaining Charge" variant="filled" /></Grid>
-                <Button color="blue" appearance="primary">
-                Search Route</Button>                     
+            <Grid container justify="center" alignIntems="center" direction="row" spacing={1}>
+                <Grid item xs={1.5}><TextField id="origin" label="Origin" variant="filled"  margin="normal" /></Grid>
+                <Grid item xs={1.5}><TextField id="destination" label="Destination" variant="filled" margin="normal" /></Grid>
+                <Grid item xs={1.5}><TextField id="charge" label="Remaining Charge" variant="filled" margin="normal" defaultValue="100" 
+                InputProps={{
+                    endAdornment: <InputAdornment position="end">%</InputAdornment>,
+                  }}/></Grid>
+                <Grid item xs={2}><TextField
+                                            id="select-car"
+                                            select
+                                            label="Car Selection"
+                                            value={car}
+                                            onChange={handleChange}
+                                            helperText="Please select your Car"
+                                            >
+                                            {cars.map((option) => (
+                                            <MenuItem key={option.value} value={option.value}>
+                                            {option.label}
+                                            </MenuItem>
+                                            ))}
+                                            </TextField></Grid>
+                <Grid item xs={1.5} ><Button id="search-button" color="blue" appearance="primary"> Search Route </Button></Grid>                    
             </Grid>
         </Grid>
 

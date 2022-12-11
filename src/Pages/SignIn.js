7023b8f9cda828ac7f094600308666023d057166
Peter Gansy <peterg@Peters-MacBook-Pro.local>
2022-12-11 02:58:45 -0500

    minor changes

--- src/Pages/SignIn.js
+++ src/Pages/SignIn.js
@@ -3,6 +3,10 @@ import '../App.css';
 import { TextField} from '@fluentui/react/lib/TextField';
 import { Stack} from '@fluentui/react/lib/Stack';
 import { DefaultButton, PrimaryButton } from '@fluentui/react/lib/Button';
+import SignUp from './SignUp';
+import { useNavigate } from "react-router-dom";
+
+
 
 const columnProps = {
     tokens: { childrenGap: 15 },
@@ -15,6 +19,13 @@ const buttonProps = {
 var stackTokens = { childrenGap: 30 };
 var stackStyles = {width: 3 };
 export default function SignIn() {
+
+    const history = useNavigate();
+  
+    const switch_page = () => {
+        console.log('clicked');
+        history.push("/signup")
+    }
     return (
     <div className='signin'>
         <br></br>
@@ -27,7 +38,7 @@ export default function SignIn() {
         </Stack>
         <br></br>
         <Stack className='login-button-placement' {...buttonProps}>
-        <DefaultButton className='login-button' text="SIGN IN"></DefaultButton>
+        <DefaultButton className='login-button' text="SIGN IN" onClick={switch_page}></DefaultButton>
         <PrimaryButton className='login-button' text="SIGN IN WITH GOOGLE"></PrimaryButton>
         </Stack>
     <h2> Never Run Out oF Charge!</h2>
--- src/Pages/SignUp.js
+++ src/Pages/SignUp.js
@@ -1,8 +1,48 @@
-import './SignUp.css';
+import React from 'react'
+import { TextField} from '@fluentui/react/lib/TextField';
+import { Stack} from '@fluentui/react/lib/Stack';
+import { DefaultButton} from '@fluentui/react/lib/Button';
 
-import React from 'react';
+const stackTokens = { childrenGap: 100 };
+const stackStyles = { root: { width: 650,
+  color: '#FFFFFF'   
+} };
+const columnProps = {
+    tokens: { childrenGap: 15 },
+    styles: { root: { width: 300 } },
+};
 
 export default function SignUp() {
-    return <h1>SignUp</h1>
+    return (
+      <div style={{ 
+        background: 'gray',
+        height:'100vh',
+        marginTop:'0px',
+        fontSize:'50px',
+        backgroundSize: 'cover',
+      }}>
+    
+    <h1 className="Heading">Profile</h1>
+    <hr></hr>
+    <h2>Enter your info!</h2>
+    
+    <hr></hr>
+    <Stack horizontal tokens={stackTokens} styles={stackStyles}>
+      <Stack {...columnProps }>
+        <TextField label="First Name" variant="outlined"/>
+        <TextField label="State" />
+        <TextField label="Street" />
+        <TextField label="Car 1 " />
+      </Stack>
+      <Stack {...columnProps}>
+        <TextField label="Last Name" />
+        <TextField label="City" />
+        <TextField label="Apt." />
+        <TextField label="Car 2" />
+      </Stack>
+      
+    </Stack>
+    <DefaultButton text="Update Profile"></DefaultButton>
+    </div>
+    );
 }
-
